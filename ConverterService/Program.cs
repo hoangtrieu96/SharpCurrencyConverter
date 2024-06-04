0ea7a00c3cd1df7ee1d3aedd39cf7ddaa2b2f5c3
Trieu Nguyen <hoangtrieunguyen.96@gmail.com>
2024-06-05 02:07:36 +1000

    + Added gRPC json transcoding to act as an API

--- ConverterService/Program.cs
+++ ConverterService/Program.cs
@@ -1,12 +1,27 @@
 using ConverterService.Services.SyncDataServices;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddAutoMapper(typeof(Program));
     builder.Services.AddScoped<ICurrencyRateClient, CurrencyRateClient>();
+    builder.Services.AddGrpc().AddJsonTranscoding();
+    builder.Services.AddGrpcReflection();
+    builder.Services.AddGrpcSwagger();
+    builder.Services.AddSwaggerGen(conf => 
+    {
+        conf.SwaggerDoc("v1", new OpenApiInfo { Title = "Sharp Currency Converter API", Version = "v1" });
+    });
 }
 
 var app = builder.Build();
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(conf => 
+    {
+        conf.SwaggerEndpoint("/swagger/v1/swagger.json", "Sharp Currency Converter API v1");
+    });
+    app.MapGrpcService<GrpcConverterService>();
+    app.MapGrpcReflectionService();
     app.Run();
 }
--- ConverterService/Services/SyncDataServices/GrpcConverterService.cs
+++ ConverterService/Services/SyncDataServices/GrpcConverterService.cs
@@ -1,20 +1,60 @@
+using CurrencyRateService;
 using Grpc.Core;
 
 namespace ConverterService.Services.SyncDataServices;
 
 public class GrpcConverterService : GrpcConverter.GrpcConverterBase
 {
+    private readonly ICurrencyRateClient _currencyRateClient;
+
+    public GrpcConverterService(ICurrencyRateClient currencyRateClient)
+    {
+        _currencyRateClient = currencyRateClient;
+    }
+
     public override async Task<ConversionResultResponse> GetConversionResult(ConversionResultRequest request, ServerCallContext context)
     {
         ValidateConversionResultRequest(request);
 
+        // Retrieve currency rates
+        (decimal fromRate, decimal toRate) = await GetRates(request.FromCurrencyCode, request.ToCurrencyCode);
+
+        // Convert amount
+        decimal conversionRate = toRate / fromRate;
+        decimal reversedConversionRate = fromRate / toRate;
+        decimal amount = decimal.Parse(request.Amount);
+
         var response = new ConversionResultResponse()
         {
-            ConvertedAmount = "0",
-            ReservedConvertedAmount = "0",
+            ConvertedAmount = (amount * conversionRate).ToString("F2"),
+            ReservedConvertedAmount = (amount * reversedConversionRate).ToString("F2")
         };
 
-        return await Task.FromResult(response);
+        return response;
+    }
+
+    private async Task<(decimal fromRate, decimal toRate)> GetRates(string fromCurrencyCode, string toCurrencyCode)
+    {
+        var rateFromToRequest = new RateFromToRequest()
+        {
+            FromCurrencyCode = fromCurrencyCode,
+            ToCurrencyCode = toCurrencyCode
+        };
+
+        var rateFromToResponse = await _currencyRateClient.GetRateFromTo(rateFromToRequest) 
+                        ?? throw new RpcException(new Status(StatusCode.NotFound, $"Currency rate not found."));
+        var rates = rateFromToResponse.ToArray();
+
+        if (rates.Length != 2)
+            throw new RpcException(new Status(StatusCode.DataLoss, $"Currency rates were missing."));
+
+        decimal? fromRate = rates.Where(r => r.CurrencyCode == fromCurrencyCode).FirstOrDefault()?.RateToUSD;
+        decimal? toRate = rates.Where(r => r.CurrencyCode == toCurrencyCode).FirstOrDefault()?.RateToUSD;
+
+        if (!fromRate.HasValue || !toRate.HasValue)
+            throw new RpcException(new Status(StatusCode.NotFound, $"Currency rate not found."));
+
+        return (fromRate.Value, toRate.Value);
     }
 
     private void ValidateConversionResultRequest(ConversionResultRequest request)
